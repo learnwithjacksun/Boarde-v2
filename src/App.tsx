ca99cd25b2400fecb65e3fe9206e11ed94837bdc
learnwithjacksun <codewithjacksun@gmail.com>
2025-05-27 12:06:13 +0100

    Add routing for SplashScreen, Auth, Home, and Tasks pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import { Toaster } from 'sonner'
-import { Home } from '@/Pages/Main'
 import { ScrollToTop } from '@/Components/UI'
+import SplashScreen from '@/Pages/SplashScreen'
+import { Auth } from '@/Pages/Auth'
+import { Home, Tasks } from '@/Pages/Main'
+
 const App = () => {
   return (
     <>
     <Toaster />
     <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
       </Routes>
     </>
   )
